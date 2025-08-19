d49da6ee15ddd7b513aecdc099ab246938f2bd74
usinha31081996 <usinha3108@gmail.com>
2025-08-19 14:22:08 +0530

    testting-variable

---
+++ variables.example/variables.tf
@@ -0,0 +1,11 @@
+variable filename{
+    type = string
+    description = "provide filename"
+    default = "cats.txt"
+}
+
+variable content{
+    type = string
+    description = "provide content"
+    default = "Pets means cat"
+}
