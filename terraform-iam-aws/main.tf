6e9edc194d5069000c231edd8dae69e0d004a95e
usinha31081996 <usinha3108@gmail.com>
2025-09-02 16:16:11 +0530

    AWS-IAM-Terraform

--- terraform-iam-aws/main.tf
+++ terraform-iam-aws/main.tf
@@ -8,6 +8,7 @@ resource "aws_iam_user" "admin-user"{
 resource "aws_iam_policy" "adminUser"{
     name = "teamLead"
     policy = file("admin-policy.json")
+
 }
 
 resource "aws_iam_user_policy_attachment" "ankit-admin-user"{
