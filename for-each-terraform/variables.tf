b5de7e1c58036250efcc11bd1280b1e8bdc2ed64
usinha31081996 <usinha3108@gmail.com>
2025-09-02 17:55:57 +0530

    for-each

---
+++ for-each-terraform/variables.tf
@@ -0,0 +1,12 @@
+variable filename{
+    type=list(string)
+    default=[
+        "cows.txt",
+        "dogs.txt",
+        "cat.txt"
+    ]
+}
+
+variable content{
+    default = "Testing terraform for each"
+} 
